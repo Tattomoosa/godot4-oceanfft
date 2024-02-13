beac03cb5987050bd1b2da28c17e44771300281e
Matt O'Tousa <mlotousa@gmail.com>
2024-02-13 03:05:12 -0800

    MotorVesselBody3D falls back to own position for null rudder/propeller

--- addons/tessarakkt.oceanfft/components/MotorVesselBody3D.gd
+++ addons/tessarakkt.oceanfft/components/MotorVesselBody3D.gd
@@ -10,7 +10,9 @@ class_name MotorVesselBody3D
 @export var thrust_power_main := 10.0
 @export var max_rudder_force := 10.0
 
+## Propeller object. If null will use the MotorVesselBody3D's position for calculations
 @export var propeller:Node3D
+## Rudder object. If null will use the MotorVesselBody3D's position for calculations
 @export var rudder:Node3D
 
 
@@ -20,19 +22,31 @@ func _process(delta):
 	if !ocean:
 		# Prints BuoyancyBody3D warning from its _physics_process, no need to warn again here
 		return
-	if ocean.get_wave_height(propeller.global_position) > propeller.global_position.y:
+	var propeller_global_position = _get_propeller_global_position()
+	if ocean.get_wave_height(propeller_global_position) > propeller_global_position.y:
 		var prop_horizontal := -global_transform.basis.z
 		prop_horizontal.y = 0.0
 		prop_horizontal = prop_horizontal.normalized()
 		var prop_dot := prop_horizontal.dot(-global_transform.basis.z)
 		
 		if Input.is_action_pressed("ship_thrust_main_forwards"):
-			apply_force(-global_transform.basis.z * thrust_power_main * prop_dot, propeller.global_position - global_position)
+			apply_force(-global_transform.basis.z * thrust_power_main * prop_dot, propeller_global_position - global_position)
 		elif Input.is_action_pressed("ship_thrust_main_backwards"):
-			apply_force(global_transform.basis.z * thrust_power_main * prop_dot, propeller.global_position - global_position)
+			apply_force(global_transform.basis.z * thrust_power_main * prop_dot, propeller_global_position - global_position)
 	
-	if ocean.get_wave_height(rudder.global_position) > rudder.global_position.y:
+	var rudder_global_position = _get_rudder_global_position()
+	if ocean.get_wave_height(rudder_global_position) > rudder_global_position.y:
 		if Input.is_action_pressed("ship_steering_main_left"):
-			apply_force(global_transform.basis.x * max_rudder_force, rudder.global_position - global_position)
+			apply_force(global_transform.basis.x * max_rudder_force, rudder_global_position - global_position)
 		elif Input.is_action_pressed("ship_steering_main_right"):
-			apply_force(-global_transform.basis.x * max_rudder_force, rudder.global_position - global_position)
+			apply_force(-global_transform.basis.x * max_rudder_force, rudder_global_position - global_position)
+
+func _get_propeller_global_position():
+	if propeller:
+		return propeller.global_position
+	return global_position
+
+func _get_rudder_global_position():
+	if rudder:
+		return rudder.global_position
+	return global_position
